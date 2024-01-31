a70d89c80c750f6da9e8bd5e8e886f07a462af62
SeongTaekPark <esulove81@gmail.com>
2024-01-31 18:20:49 +0900

    SnackBar

---
+++ app/src/main/java/io/papamangto/studyjetpackcompose/SnackBarActivity.kt
@@ -0,0 +1,57 @@
+package io.papamangto.studyjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import kotlinx.coroutines.launch
+
+@ExperimentalComposeUiApi
+class SnackBarActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val (text, setValue) = remember {
+                mutableStateOf("")
+            }
+
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
+            
+            Scaffold(
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue,
+                    )
+                    Button(onClick = {
+                        keyboardController?.hide()
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $text")
+                        }
+                    }) {
+                        Text(text = "클릭!!")
+                    }
+                }
+            }
+        }
+    }
+}
